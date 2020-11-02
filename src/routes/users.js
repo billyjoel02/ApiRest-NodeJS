299d78ec4af94a140ad10a2eb08f6f007a0d3b1c
billy0299 <joelbraco0299@gmail.com>
2020-11-02 23:44:15 +0100

    añadiendo las apis comunes get, put, delete, put

---
+++ src/routes/users.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = new Router();
+const fetch = require('node-fetch');
+
+
+router.get('/', async (req, res) => {
+    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    res.json(users);
+
+
+});
+
+module.exports = router;
